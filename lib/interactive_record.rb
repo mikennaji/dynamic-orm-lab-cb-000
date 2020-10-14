39c7d516a1c545fd5d61f56e1b684083f80ba2ef
Michael Nnaji <mnnaji3@uwo.ca>
2020-10-14 23:41:31 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,4 +7,16 @@ def self.table_name
   self.to_s.downcase.pluralize
 end
 
+def self.column_names
+  sql = "PRAGMA table_info('#{table_name}')"
+  table_info = DB[:conn].execute(sql)
+  column_names
+    table_info.each do |column|
+      column_names << column["name"]
+    end 
+  column_names.comapct 
 end
+
+end 
+
+
